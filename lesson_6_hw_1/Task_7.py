85051739e30440778c9864fc0419cec1fe2a657c
TetianaMyroshnychenk <miroshnichenkott23@gmail.com>
2022-10-08 19:23:24 +0300

    HW was fixed

--- lesson_6_hw_1/Task_7.py
+++ lesson_6_hw_1/Task_7.py
@@ -2,24 +2,20 @@ import random
 
 
 def main():
+    guess_the_number()
+
+
+def guess_the_number():
     number = random.randint(1, 10)
     user_number = int(input('Enter a number: '))
-    guess_the_number(number, user_number)
-
-
-def guess_the_number(number, user_number):
-    if user_number == number:
-        print('Congratulation!')
-    else:
-        while user_number != number:
-            if user_number > number:
-                print('Your number bigger than target')
-                user_number = int(input('Enter a number: '))
-            elif user_number < number:
-                print('Your number smaller than target')
-                user_number = int(input('Enter a number: '))
-
-        print('Congratulation!')
+    while user_number != number:
+        if user_number > number:
+            print('Your number bigger than target')
+            user_number = int(input('Enter a number: '))
+        elif user_number < number:
+            print('Your number smaller than target')
+            user_number = int(input('Enter a number: '))
+    print('Congratulation!')
 
 
 if __name__ == '__main__':
