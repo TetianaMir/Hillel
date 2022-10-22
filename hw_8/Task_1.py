64189e5e6e0615b1a488834ddc6cba9dd29c987f
TetianaMyroshnychenk <miroshnichenkott23@gmail.com>
2022-10-22 23:54:40 +0300

    The issues were fixed (the useless code lines were deleted)

--- hw_8/Task_1.py
+++ hw_8/Task_1.py
@@ -10,18 +10,11 @@ def expected(*expected_types):
         @functools.wraps(func)
         def wrapper(*args, **kwargs):
             result = func(*args, **kwargs)
-            if is_boolean_checker(result):
-                result = bool
-            try:
-                if not isinstance(result, expected_types):
-                    raise UnexpectedTypeException("It's unexpected type")
-                else:
-                    print("The result of functions:", func(*args, **kwargs), "is :", expected_types)
-            except UnexpectedTypeException as exc:
-                print(exc)
-
+            if not isinstance(result, expected_types):
+                raise UnexpectedTypeException("It's unexpected type")
+            else:
+                print("The result of functions:", func(*args, **kwargs), "is :", expected_types)
         return wrapper
-
     return decorator
 
 
@@ -30,14 +23,8 @@ def func(value):
     return value
 
 
-def is_boolean_checker(type):
-    return isinstance(type, bool)
-
-
 def main():
-    func(7.6)  # unexpected type
-    func(7)  # expected
-    func(True)  # unexpected type
+    func("str")  # expected
 
 
 if __name__ == '__main__':
